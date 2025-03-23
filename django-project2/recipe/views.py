cfaf5fe67361a25febf70538ad91c1d4d2ed4adf
Matheus Henrique da silva Almeida <123656754+MtsAlmeida2023@users.noreply.github.com>
2025-03-23 18:12:10 -0300

    Remove
    Remove views 'contato' and 'sobre'

--- django-project2/recipe/views.py
+++ django-project2/recipe/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.contrib import admin
 # Create your views here.
+
+def home(request):
+    return render(request, 'recipe/home.html', context={
+        'name': 'Luiz Otavio'
+    })
+
